6b00bde428da338f54d70e2f4638a739de3039c9
gabrmanh <gmanhani@yahoo.com>
2024-08-13 02:44:05 -0300

    feat: created tentative UF enum

---
+++ app/src/main/java/com/example/cadastro/domain/UnidadeFederal.kt
@@ -0,0 +1,31 @@
+package com.example.cadastro.domain
+
+enum class UnidadeFederal(val abbreviation: String) {
+    ACRE("AC"),
+    ALAGOAS("AL"),
+    AMAPA("AP"),
+    AMAZONAS("AM"),
+    BAHIA("BA"),
+    CEARA("CE"),
+    DISTRICT_FEDERAL("DF"),
+    ESPIRITO_SANTO("ES"),
+    GOIAS("GO"),
+    MARANHAO("MA"),
+    MATO_GROSSO("MT"),
+    MATO_GROSSO_DO_SUL("MS"),
+    MINAS_GERAIS("MG"),
+    PARA("PA"),
+    PARAIBA("PB"),
+    PARANA("PR"),
+    PERNAMBUCO("PE"),
+    PIAUI("PI"),
+    RIO_DE_JANEIRO("RJ"),
+    RIO_GRANDE_DO_NORTE("RN"),
+    RIO_GRANDE_DO_SUL("RS"),
+    RONDONIA("RO"),
+    RORAIMA("RR"),
+    SANTA_CATARINA("SC"),
+    SAO_PAULO("SP"),
+    SERGIPE("SE"),
+    TOCANTINS("TO")
+}
