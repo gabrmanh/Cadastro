595252c0d19b06bef55fa7146603eb1da53ead4c
gabrmanh <gmanhani@yahoo.com>
2024-08-13 20:25:15 -0300

    feat: save button functionality

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.cadastro
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.cadastro.databinding.ActivityMainBinding
+import com.example.cadastro.domain.Genero
+import com.example.cadastro.domain.UnidadeFederal
+import com.example.cadastro.domain.Usuario
 
 class MainActivity : AppCompatActivity() {
     lateinit var amb: ActivityMainBinding
@@ -15,12 +21,34 @@ class MainActivity : AppCompatActivity() {
         amb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amb.root)
 
+        val unidadesFederais = mutableListOf<String>()
+        UnidadeFederal.entries.forEach { unidadesFederais.add(it.toString()) }
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, unidadesFederais)
+        amb.unidadeSp.adapter = adapter
+
+        val emailList = mutableListOf<String>()
+
         amb.limparBt.setOnClickListener {
             limpar()
         }
 
         amb.salvarBt.setOnClickListener {
+            val genero = findViewById<RadioButton>(amb.generoRg.checkedRadioButtonId).text.toString()
+            val uf = amb.unidadeSp.selectedItem.toString()
 
+            if(amb.emailCb.isChecked) emailList.add(amb.emailEt.text.toString())
+
+            val user = Usuario(
+                amb.nomeEt.text.toString(),
+                amb.telefoneEt.text.toString(),
+                amb.emailEt.text.toString(),
+                Genero.fromRadio(genero),
+                amb.cidadeEt.text.toString(),
+                UnidadeFederal.valueOf(uf)
+            )
+
+            Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 
