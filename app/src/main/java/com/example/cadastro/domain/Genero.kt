37cc56fc4273a012ddc1520efc8f063c2ea33007
gabrmanh <gmanhani@yahoo.com>
2024-08-13 20:24:50 -0300

    refactor: gender now found by radio value

--- app/src/main/java/com/example/cadastro/domain/Genero.kt
+++ app/src/main/java/com/example/cadastro/domain/Genero.kt
@@ -1,8 +1,20 @@
 package com.example.cadastro.domain
 
-enum class Genero {
-    HOMEM,
-    MULHER,
-    NAOBINARIO,
-    OUTRO
+enum class Genero(val valor: String) {
+    HOMEM("Homem"),
+    MULHER("Mulher"),
+    NAOBINARIO("Não-binário"),
+    OUTRO("Outro");
+
+    companion object {
+        fun fromRadio(radioValue: String): Genero? {
+            return when (radioValue) {
+                "Homem" -> HOMEM
+                "Mulher" -> MULHER
+                "Não-binário" -> NAOBINARIO
+                "Outro" -> OUTRO
+                else -> null
+            }
+        }
+    }
 }
